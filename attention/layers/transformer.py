4e5ab6d5aaee908653716b182fe014abee7b7107
bohyun-shin <bohyun.shin@linecorp.com>
2024-04-08 07:09:08 +0900

    Add `EncoderLayer`

--- attention/layers/transformer.py
+++ attention/layers/transformer.py
@@ -12,13 +12,29 @@ class EncoderLayer(nn.Module):
                  d_v,
                  dropout=0.1):
         super(EncoderLayer, self).__init__()
-        self.slf_attn = MultiHeadAttention()
-        self.pos_ffn = PositionwiseFeedForwrd()
+        self.slf_attn = MultiHeadAttention(
+            n_head=n_head,
+            d_model=d_model,
+            d_emb=d_model,
+            d_k=d_k,
+            d_v=d_v,
+            dropout=dropout
+        )
+        self.pos_ffn = PositionwiseFeedForwrd(
+            d_in=d_model,
+            d_hid=d_inner,
+            dropout=dropout
+        )
 
-    def forward(self):
-        enc_output = self.slf_attn()
-        enc_output = self.pos_ffn()
-        return enc_output
+    def forward(self, enc_input, slf_attn_mask=None):
+        enc_output = self.slf_attn(
+            q=enc_input,
+            k=enc_input,
+            v=enc_input,
+            mask=slf_attn_mask
+        )
+        enc_output, enc_slf_attn = self.pos_ffn(enc_output)
+        return enc_output, enc_slf_attn
 
 
 class DecoderLayer(nn.Module):

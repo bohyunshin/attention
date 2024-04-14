ac4b9bd0e4b7cd18a4510b9432089f49106b5a9e
bohyun-shin <bohyun.shin@linecorp.com>
2024-04-15 06:37:37 +0900

    Modify to double quote

--- attention/train.py
+++ attention/train.py
@@ -1,8 +1,3 @@
-'''
-This script handles the training process.
-'''
-
-import argparse
 import math
 import time
 import dill as pickle
@@ -17,17 +12,14 @@ sys.path.append(os.getcwd())
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
-from torchtext.data import Field, Dataset, BucketIterator
-from torchtext.datasets import TranslationDataset
+from torchtext.data import Dataset, BucketIterator
 
 from attention.tools.arg_parse import parse_args
 from attention.constants import PAD_WORD
 from attention.optimizer.scheduled_optimizer import ScheduledOptim
 
-__author__ = "Yu-Hsiang Huang"
-
 def cal_performance(pred, gold, trg_pad_idx, smoothing=False):
-    ''' Apply label smoothing if needed '''
+    """ Apply label smoothing if needed """
 
     loss = cal_loss(pred, gold, trg_pad_idx, smoothing=smoothing)
 
@@ -41,7 +33,7 @@ def cal_performance(pred, gold, trg_pad_idx, smoothing=False):
 
 
 def cal_loss(pred, gold, trg_pad_idx, smoothing=False):
-    ''' Calculate cross entropy loss, apply label smoothing if needed. '''
+    """ Calculate cross entropy loss, apply label smoothing if needed. """
 
     gold = gold.contiguous().view(-1)
 
@@ -57,7 +49,7 @@ def cal_loss(pred, gold, trg_pad_idx, smoothing=False):
         loss = -(one_hot * log_prb).sum(dim=1)
         loss = loss.masked_select(non_pad_mask).sum()  # average later
     else:
-        loss = F.cross_entropy(pred, gold, ignore_index=trg_pad_idx, reduction='sum')
+        loss = F.cross_entropy(pred, gold, ignore_index=trg_pad_idx, reduction="sum")
     return loss
 
 
@@ -73,12 +65,12 @@ def patch_trg(trg, pad_idx):
 
 
 def train_epoch(model, training_data, optimizer, opt, device, smoothing):
-    ''' Epoch operation in training phase'''
+    """ Epoch operation in training phase"""
 
     model.train()
     total_loss, n_word_total, n_word_correct = 0, 0, 0
 
-    desc = '  - (Training)   '
+    desc = "  - (Training)   "
     for batch in tqdm(training_data, mininterval=2, desc=desc, leave=False):
 
         # prepare data
@@ -106,12 +98,12 @@ def train_epoch(model, training_data, optimizer, opt, device, smoothing):
 
 
 def eval_epoch(model, validation_data, device, opt):
-    ''' Epoch operation in evaluation phase '''
+    """ Epoch operation in evaluation phase """
 
     model.eval()
     total_loss, n_word_total, n_word_correct = 0, 0, 0
 
-    desc = '  - (Validation) '
+    desc = "  - (Validation) "
     with torch.no_grad():
         for batch in tqdm(validation_data, mininterval=2, desc=desc, leave=False):
 
@@ -135,79 +127,79 @@ def eval_epoch(model, validation_data, device, opt):
 
 
 def train(model, training_data, validation_data, optimizer, device, opt):
-    ''' Start training '''
+    """ Start training """
 
     # Use tensorboard to plot curves, e.g. perplexity, accuracy, learning rate
     if opt.use_tb:
         print("[Info] Use Tensorboard")
         from torch.utils.tensorboard import SummaryWriter
-        tb_writer = SummaryWriter(log_dir=os.path.join(opt.output_dir, 'tensorboard'))
+        tb_writer = SummaryWriter(log_dir=os.path.join(opt.output_dir, "tensorboard"))
 
-    log_train_file = os.path.join(opt.output_dir, 'train.log')
-    log_valid_file = os.path.join(opt.output_dir, 'valid.log')
+    log_train_file = os.path.join(opt.output_dir, "train.log")
+    log_valid_file = os.path.join(opt.output_dir, "valid.log")
 
-    print('[Info] Training performance will be written to file: {} and {}'.format(
+    print("[Info] Training performance will be written to file: {} and {}".format(
         log_train_file, log_valid_file))
 
-    with open(log_train_file, 'w') as log_tf, open(log_valid_file, 'w') as log_vf:
-        log_tf.write('epoch,loss,ppl,accuracy\n')
-        log_vf.write('epoch,loss,ppl,accuracy\n')
+    with open(log_train_file, "w") as log_tf, open(log_valid_file, "w") as log_vf:
+        log_tf.write("epoch,loss,ppl,accuracy\n")
+        log_vf.write("epoch,loss,ppl,accuracy\n")
 
     def print_performances(header, ppl, accu, start_time, lr):
-        print('  - {header:12} ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, lr: {lr:8.5f}, '\
-              'elapse: {elapse:3.3f} min'.format(
+        print("  - {header:12} ppl: {ppl: 8.5f}, accuracy: {accu:3.3f} %, lr: {lr:8.5f}, "\
+              "elapse: {elapse:3.3f} min".format(
                   header=f"({header})", ppl=ppl,
                   accu=100*accu, elapse=(time.time()-start_time)/60, lr=lr))
 
     #valid_accus = []
     valid_losses = []
     for epoch_i in range(opt.epoch):
-        print('[ Epoch', epoch_i, ']')
+        print("[ Epoch", epoch_i, "]")
 
         start = time.time()
         train_loss, train_accu = train_epoch(
             model, training_data, optimizer, opt, device, smoothing=opt.label_smoothing)
         train_ppl = math.exp(min(train_loss, 100))
         # Current learning rate
-        lr = optimizer._optimizer.param_groups[0]['lr']
-        print_performances('Training', train_ppl, train_accu, start, lr)
+        lr = optimizer._optimizer.param_groups[0]["lr"]
+        print_performances("Training", train_ppl, train_accu, start, lr)
 
         start = time.time()
         valid_loss, valid_accu = eval_epoch(model, validation_data, device, opt)
         valid_ppl = math.exp(min(valid_loss, 100))
-        print_performances('Validation', valid_ppl, valid_accu, start, lr)
+        print_performances("Validation", valid_ppl, valid_accu, start, lr)
 
         valid_losses += [valid_loss]
 
-        checkpoint = {'epoch': epoch_i, 'settings': opt, 'model': model.state_dict()}
+        checkpoint = {"epoch": epoch_i, "settings": opt, "model": model.state_dict()}
 
-        if opt.save_mode == 'all':
-            model_name = 'model_accu_{accu:3.3f}.chkpt'.format(accu=100*valid_accu)
+        if opt.save_mode == "all":
+            model_name = "model_accu_{accu:3.3f}.chkpt".format(accu=100*valid_accu)
             torch.save(checkpoint, model_name)
-        elif opt.save_mode == 'best':
-            model_name = 'model.chkpt'
+        elif opt.save_mode == "best":
+            model_name = "model.chkpt"
             if valid_loss <= min(valid_losses):
                 torch.save(checkpoint, os.path.join(opt.output_dir, model_name))
-                print('    - [Info] The checkpoint file has been updated.')
+                print("    - [Info] The checkpoint file has been updated.")
 
-        with open(log_train_file, 'a') as log_tf, open(log_valid_file, 'a') as log_vf:
-            log_tf.write('{epoch},{loss: 8.5f},{ppl: 8.5f},{accu:3.3f}\n'.format(
+        with open(log_train_file, "a") as log_tf, open(log_valid_file, "a") as log_vf:
+            log_tf.write("{epoch},{loss: 8.5f},{ppl: 8.5f},{accu:3.3f}\n".format(
                 epoch=epoch_i, loss=train_loss,
                 ppl=train_ppl, accu=100*train_accu))
-            log_vf.write('{epoch},{loss: 8.5f},{ppl: 8.5f},{accu:3.3f}\n'.format(
+            log_vf.write("{epoch},{loss: 8.5f},{ppl: 8.5f},{accu:3.3f}\n".format(
                 epoch=epoch_i, loss=valid_loss,
                 ppl=valid_ppl, accu=100*valid_accu))
 
         if opt.use_tb:
-            tb_writer.add_scalars('ppl', {'train': train_ppl, 'val': valid_ppl}, epoch_i)
-            tb_writer.add_scalars('accuracy', {'train': train_accu*100, 'val': valid_accu*100}, epoch_i)
-            tb_writer.add_scalar('learning_rate', lr, epoch_i)
+            tb_writer.add_scalars("ppl", {"train": train_ppl, "val": valid_ppl}, epoch_i)
+            tb_writer.add_scalars("accuracy", {"train": train_accu*100, "val": valid_accu*100}, epoch_i)
+            tb_writer.add_scalar("learning_rate", lr, epoch_i)
 
 def main():
-    '''
+    """
     Usage:
     python train.py -data_pkl m30k_deen_shr.pkl -log m30k_deen_shr -embs_share_weight -proj_share_weight -label_smoothing -output_dir output -b 256 -warmup 128000
-    '''
+    """
     opt = parse_args()
 
     opt.cuda = not opt.no_cuda
@@ -223,19 +215,19 @@ def main():
         random.seed(opt.seed)
 
     if not opt.output_dir:
-        print('No experiment result will be saved.')
+        print("No experiment result will be saved.")
         raise
 
     if not os.path.exists(opt.output_dir):
         os.makedirs(opt.output_dir)
 
     if opt.batch_size < 2048 and opt.n_warmup_steps <= 4000:
-        print('[Warning] The warmup steps may be not enough.\n'\
-              '(sz_b, warmup) = (2048, 4000) is the official setting.\n'\
-              'Using smaller batch w/o longer warmup may cause '\
-              'the warmup stage ends with only little data trained.')
+        print("[Warning] The warmup steps may be not enough.\n"\
+              "(sz_b, warmup) = (2048, 4000) is the official setting.\n"\
+              "Using smaller batch w/o longer warmup may cause "\
+              "the warmup stage ends with only little data trained.")
 
-    device = torch.device('cuda' if opt.cuda else 'cpu')
+    device = torch.device("cuda" if opt.cuda else "cpu")
 
     #========= Loading Dataset =========#
 
@@ -259,24 +251,24 @@ def main():
 
 def prepare_dataloaders(opt, device):
     batch_size = opt.batch_size
-    data = pickle.load(open(opt.data_pkl, 'rb'))
+    data = pickle.load(open(opt.data_pkl, "rb"))
 
-    opt.max_token_seq_len = data['settings'].max_len
-    opt.src_pad_idx = data['vocab']['src'].vocab.stoi[PAD_WORD]
-    opt.trg_pad_idx = data['vocab']['trg'].vocab.stoi[PAD_WORD]
+    opt.max_token_seq_len = data["settings"].max_len
+    opt.src_pad_idx = data["vocab"]["src"].vocab.stoi[PAD_WORD]
+    opt.trg_pad_idx = data["vocab"]["trg"].vocab.stoi[PAD_WORD]
 
-    opt.src_vocab_size = len(data['vocab']['src'].vocab)
-    opt.trg_vocab_size = len(data['vocab']['trg'].vocab)
+    opt.src_vocab_size = len(data["vocab"]["src"].vocab)
+    opt.trg_vocab_size = len(data["vocab"]["trg"].vocab)
 
     #========= Preparing Model =========#
     if opt.embs_share_weight:
-        assert data['vocab']['src'].vocab.stoi == data['vocab']['trg'].vocab.stoi, \
-            'To sharing word embedding the src/trg word2idx table shall be the same.'
+        assert data["vocab"]["src"].vocab.stoi == data["vocab"]["trg"].vocab.stoi, \
+            "To sharing word embedding the src/trg word2idx table shall be the same."
 
-    fields = {'src': data['vocab']['src'], 'trg':data['vocab']['trg']}
+    fields = {"src": data["vocab"]["src"], "trg":data["vocab"]["trg"]}
 
-    train = Dataset(examples=data['train'], fields=fields)
-    val = Dataset(examples=data['valid'], fields=fields)
+    train = Dataset(examples=data["train"], fields=fields)
+    val = Dataset(examples=data["valid"], fields=fields)
 
     train_iterator = BucketIterator(train, batch_size=batch_size, device=device, train=True)
     val_iterator = BucketIterator(val, batch_size=batch_size, device=device)
@@ -284,5 +276,5 @@ def prepare_dataloaders(opt, device):
     return train_iterator, val_iterator
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

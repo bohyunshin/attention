267d480af119ad13b883cfd443f7323c08f27aca
bohyun-shin <bohyun.shin@linecorp.com>
2024-04-23 08:35:39 +0900

    Add test script for bert layers

--- attention/layers/bert.py
+++ attention/layers/bert.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 from attention.sublayers.bert import MultiHeadAttention, PositionwiseFeedForward
 
 
@@ -10,23 +11,25 @@ class EncoderLayer(nn.Module):
                  d_k,
                  d_v,
                  d_hid,
-                 activation=nn.GELU,
+                 activation=F.gelu,
                  dropout=0.1
                  ):
         super().__init__()
 
+        # dropout, layers normalization included
         self.slf_attention = MultiHeadAttention(n_head=n_head,
                                                 d_model=d_model,
                                                 d_emb=d_emb,
                                                 d_k=d_k,
                                                 d_v=d_v,
                                                 dropout=dropout)
+        # dropout, layers normalization included
         self.ffw = PositionwiseFeedForward(d_in=d_model,
                                            d_hid=d_hid,
                                            activation=activation,
                                            dropout=dropout)
 
-    def forward(self, enc_input, mask):
-        x = self.slf_attention(enc_input, enc_input, enc_input, mask)
-        attn = self.ffw(x)
-        return attn
+    def forward(self, enc_input, mask=None):
+        enc_output, attn = self.slf_attention(enc_input, enc_input, enc_input, mask)
+        enc_output = self.ffw(enc_output)
+        return enc_output, attn
--- test/test_bert.py
+++ test/test_bert.py
@@ -3,7 +3,9 @@ import sys
 sys.path.append(os.getcwd())
 
 import torch
+import torch.nn as nn
 from attention.embedding.bert import BERTEmbedding
+from attention.layers.bert import EncoderLayer
 from attention.tools.utils import asserting
 
 def test_bert_embedding_shape():
@@ -20,3 +22,28 @@ def test_bert_embedding_shape():
                                    seq_len=seq_len)
     embedding = bert_embedding(rand_word_vec, segment_label)
     asserting((batch_size, seq_len, embed_size), embedding.shape)
+
+def test_encoder_layer_shape():
+    d_model = 512
+    d_inner = 256
+    n_head = 8
+    d_k = int(d_model // n_head)
+    d_v = d_k
+    dropout = 0.1
+    batch_size = 1
+    seq_len = 200
+    x = torch.rand((batch_size, seq_len, d_model))
+    encoder_layer = EncoderLayer(
+        d_model=d_model,
+        d_hid=d_inner,
+        d_emb=d_model,
+        n_head=n_head,
+        d_k=d_k,
+        d_v=d_v,
+        dropout=dropout
+    )
+
+    enc_output, enc_slf_attn = encoder_layer(x)
+
+    asserting((batch_size, seq_len, d_model), enc_output.shape)
+    asserting((batch_size, n_head, seq_len, seq_len), enc_slf_attn.shape)

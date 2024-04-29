6fba2bbc05a454acad46ef40c54bf1ed7ce042a2
bohyun-shin <bohyun.shin@linecorp.com>
2024-04-29 21:41:26 +0900

    Add bert train pipeline

--- attention/train/bert.py
+++ attention/train/bert.py
@@ -15,12 +15,31 @@ class Train(TrainBase):
         self.criterion = torch.nn.NLLLoss(ignore_index=0)
 
     def cal_loss(self, pred, gold):
-        loss = self.criterion(pred, gold)
-        return loss
+        next_sent_output, mask_lm_output = pred
+
+        next_loss = self.criterion(next_sent_output, gold["is_next"])
+        mask_loss = self.criterion(mask_lm_output, gold["bert_label"])
+        return next_loss + mask_loss
 
     def cal_performance(self, pred, gold):
-        pass
+        next_sent_output, mask_lm_output = pred
+        loss = self.cal_loss(pred, gold)
+
+        # next sentence prediction accuracy
+        correct = next_sent_output.argmax(dim=-1).eq(gold["is_next"]).sum().item() # 0 or 1
+        element = gold["is_next"].nelement() # 1
+
+        return loss, correct, element
+
 
     def get_data_from_batch(self, batch, arg, device):
         data = {key: value.to(self.device) for key, value in batch.items()}
-        return data, data, data
+        model_input = {
+            "x":data["bert_input"],
+            "segment_label":data["segment_label"]
+        }
+        gold = {
+            "bert_label":data["bert_label"],
+            "is_next":data["is_next"]
+        }
+        return model_input, gold
